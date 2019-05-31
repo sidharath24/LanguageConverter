5f8eebae70d74c31d67b97f827991fac734cc9c8
Sidharath <sid.aggarwal.31@gmail.com>
2019-05-31 03:04:11 -0400

    Added all the Activities

--- app/src/main/java/com/example/owner/languageconverter/ColorsActivity.java
+++ app/src/main/java/com/example/owner/languageconverter/ColorsActivity.java
@@ -2,6 +2,10 @@ package com.example.owner.languageconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother ", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        WordAdapter adapter = new WordAdapter(this,words);
+
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
     }
 }

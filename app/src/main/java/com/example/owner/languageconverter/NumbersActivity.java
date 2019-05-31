12894b6b6e269fd00c9ff5861f7938e83fd1fcc2
Sidharath <sid.aggarwal.31@gmail.com>
2019-05-31 02:02:12 -0400

    Added new activities

---
+++ app/src/main/java/com/example/owner/languageconverter/NumbersActivity.java
@@ -0,0 +1,13 @@
+package com.example.owner.languageconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+    }
+}

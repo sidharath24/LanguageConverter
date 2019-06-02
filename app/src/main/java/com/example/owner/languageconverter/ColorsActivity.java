99cfec16db1f61c1dbbaff2c8260526135bb5bd7
Sidharath <sid.aggarwal.31@gmail.com>
2019-06-02 00:47:32 -0400

    Added the release method

--- app/src/main/java/com/example/owner/languageconverter/ColorsActivity.java
+++ app/src/main/java/com/example/owner/languageconverter/ColorsActivity.java
@@ -13,13 +13,20 @@ import java.util.ArrayList;
 public class ColorsActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    private MediaPlayer.OnCompletionListener mCompleteListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
 
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red,R.raw.color_red));
+        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
         words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
         words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
         words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
@@ -30,7 +37,7 @@ public class ColorsActivity extends AppCompatActivity {
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(this, words,R.color.category_colors);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -41,16 +48,32 @@ public class ColorsActivity extends AppCompatActivity {
         // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener (new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word word = words.get(position);
 
-                    mediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getmAudioResourceId());
-                
+                releaseMediaPlayer();
+
+                mediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getmAudioResourceId());
+
                 mediaPlayer.start();
+
+                mediaPlayer.setOnCompletionListener(mCompleteListener);
             }
         });
+    }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
